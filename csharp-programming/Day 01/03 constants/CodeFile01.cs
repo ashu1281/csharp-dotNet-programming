5a6dae7cacd02f0bfe574741d281c5a1e4fdd600
ashu1281 <gaiwadashish628@gmail.com>
2023-03-01 13:28:56 +0530

    constants

---
+++ csharp-programming/Day 01/03 constants/CodeFile01.cs
@@ -0,0 +1,45 @@
+﻿namespace csharp_programming {
+    class Program {
+        static void Main() {
+            // integer constants
+            // 101; constant in decimal numbering system
+            // 016; constant in octal numbering system
+            // 0x0019fe46; constant in hexadecimal numbering system
+            // 0X0019FE46; constant in hexadecimal numbering system
+            // 0b101; constant in binary numbering system
+            // 0B101; constant in binary numbering system
+
+            // floating point constants
+            // 3.14145; double constant
+            // 312569E-5; double constant
+            // 784f; float constant
+            // 123.222F; float constant
+
+            // character constants
+            // 'a'; character constant
+            // '\u0061'; in unicode
+            // '\n'; escape sequence
+            // '\\'; \ character
+            // '\''; ' character
+            // '\"'; " character
+            // '\b'; Backspace
+            // '\a'; Alert or Bell
+            // '\n'; New Line
+            // '\f'; Form Feed
+            // '\r'; Carriage Return
+            // '\v'; Vertical Tab
+
+            // string literals
+            // "Hello World!"; string literal
+            // @"Hello World!"; //verbatim string
+
+            // boolean constants
+            // true; boolean constant
+            // false; boolean constant
+        }
+    }
+}
+
+/*
+ * Input above literals and constants in watch window and observe their data types in type column.
+ */
