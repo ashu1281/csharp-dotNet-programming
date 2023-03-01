6744f03a94cf802871033929b48c8264f8889024
ashu1281 <gaiwadashish628@gmail.com>
2023-03-01 10:55:10 +0530

    Add project files.

---
+++ csharp-programming/Day 01/01 main function/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_programming
+{
+    internal class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            return 101;
+        }
+    }
+}
+
+/*
+ * 'Main' is a function.
+ * It is also an entry point function to C# program.
+ * C# is case sensitive language, hence entry point function must be written as 'Main' and not in any other form.
+ * Main can be defined as an entry point function in both class and struct.
+ * For C# source to get compile:
+ * - It is not compulsory for C# source file to end with extension .cs.
+ * - It is not compulsory for C# source file to have name same as the public class contained in it.
+ * If more than one class has Main method then use /main compiler switch to specify which Main
+ * method to be selected as an entry point function.
+ * Value returned from an entry point function is known as exit code and is stored in
+ * ERRORLEVEL environment variable.
+ */
---
+++ csharp-programming/Day 01/02 top level statements/CodeFile01.cs
@@ -0,0 +1,12 @@
+﻿int a = 1, b = 2, c = 0;
+c = a + b;
+System.Console.WriteLine("c = {0}", c);
+
+/*
+ * Starting in C# 9, you don't have to explicitly include a Main method in a console application project.
+ * Instead, you can use the top-level statements feature to minimize the code you have to write.
+ * In this case, the compiler generates a class and Main method entry point for the application.
+ * A project can have only one file with top-level statements.
+ * Putting top-level statements in more than one file in a project results in the following compiler error:
+ * CS8802 Only one compilation unit can have top-level statements.
+ */
