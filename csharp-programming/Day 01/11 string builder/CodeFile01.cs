caad58042c81a1c97f33894b2235b102a35b5bc7
ashu1281 <gaiwadashish628@gmail.com>
2023-03-01 13:31:04 +0530

    string builder

---
+++ csharp-programming/Day 01/11 string builder/CodeFile01.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace csharp_programming {
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder sb = new StringBuilder("Hello World,");
+            sb.Append(" What a sunny day today!\n");
+            sb.AppendFormat("It's {0}\u00b0 celsius temperature outside.", 40);
+            sb.Replace("40", "104");
+            sb.Remove(sb.ToString().IndexOf("celsius "), 7);
+            sb.Insert(sb.ToString().IndexOf("temperature"), "fahrenheit ");
+            string result = sb.ToString();
+        }
+    }
+}
