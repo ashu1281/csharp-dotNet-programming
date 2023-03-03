56af97a505a380f2f477d7f0231560eee6d88e98
ashu1281 <gaiwadashish628@gmail.com>
2023-03-03 12:46:30 +0530

    variadic method

---
+++ csharp-programming/Day 03/06 methods/01 variadic method/CodeFile01.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp_programming {
+    class Math {
+
+      //public int Sum(params object[] n) { // int can have heterogenious values
+        public int Sum(params int[] n) { // int have only homogenious values
+            int result = 0;
+            foreach(int t in n)
+                result += t;
+            return result;
+        }
+    }
+    class Program {
+        static void Main() {
+            Math m = new Math();
+            int n = m.Sum(1, 5);//6
+            n = m.Sum(1, 5, 7);
+           // n = Math.Sum(1, 5, 7); not possible
+
+
+        }
+    }
+}
+/*
+ * Non-static methos (also known as instance method) can be called using instace only.
+ * It cannot be called using class.
+ */
---
+++ csharp-programming/Day 03/06 methods/01 variadic method/CodeFile02.cs
@@ -0,0 +1,32 @@
+﻿namespace csharp_programming
+{
+    class Math
+    {
+
+        public static int Sum(params int[] n)
+        { // int have only homogenious values
+            int result = 0;
+            foreach (int t in n)
+                result += t;
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Math m = new Math();
+            int n = Math.Sum(1, 5);//6
+            //n = m.Sum(1, 5, 7); //not possible
+            
+
+
+        }
+    }
+}
+
+/*
+ * In c#, static method(class method) can be called using class only.
+ * It cannot be called using instance of a class.
+ */
+
