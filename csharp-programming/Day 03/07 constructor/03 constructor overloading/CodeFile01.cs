2a2d0c529f5cd3839aa5393302fa52477ba3e00b
ashu1281 <gaiwadashish628@gmail.com>
2023-03-03 13:20:31 +0530

    constructor

---
+++ csharp-programming/Day 03/07 constructor/03 constructor overloading/CodeFile01.cs
@@ -0,0 +1,17 @@
+﻿namespace csharp_programming {
+    class Integer {
+        int value;
+        public Integer() {
+            this.value = 1;
+        }
+        public Integer(int value) {
+            this.value = value;
+        }
+    }
+    class Program {
+        static void Main() {
+            Integer i = new Integer();
+            Integer j = new Integer(1);
+        }
+    }
+}
---
+++ csharp-programming/Day 03/07 constructor/04 constructor calling another constructor/CodeFile01.cs
@@ -0,0 +1,16 @@
+﻿namespace csharp_programming {
+    class Integer {
+        int value;
+        public Integer() : this(1) { //here by using "sthis" default constructor called to para constructor
+        }
+        public Integer(int value) {
+            this.value = value;
+        }
+    }
+    class Program {
+        static void Main() {
+            Integer i = new Integer();
+            Integer j = new Integer(1);
+        }
+    }
+}
