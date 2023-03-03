c79b64b705215d9e63bdcece07f61cd1d695d5f1
ashu1281 <gaiwadashish628@gmail.com>
2023-03-03 13:20:00 +0530

    defualt consturtor

---
+++ csharp-programming/Day 03/07 constructor/01 default constructor/CodeFile01.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp_programming {
+    class Integer {
+        int value;
+        public Integer() {
+            value = 1;
+        }
+    }
+    class Program {
+        static void Main() {
+            Integer i = new Integer();
+        }
+    }
+}
