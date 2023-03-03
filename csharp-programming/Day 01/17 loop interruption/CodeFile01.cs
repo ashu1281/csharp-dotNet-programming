7951acf1f49a56e66ceaa19e937f4feaa9dd58ce
ashu1281 <gaiwadashish628@gmail.com>
2023-03-03 11:13:45 +0530

    loop interruption

---
+++ csharp-programming/Day 01/17 loop interruption/CodeFile01.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace csharp_programming {
+    class Program {
+        static void Main() {
+            string input;
+
+            Console.Write("Enter first integer: ");
+            input = Console.ReadLine();
+            int a = Convert.ToInt32(input);
+
+            Console.Write("Enter second integer: ");
+            input = Console.ReadLine();
+            int b = Convert.ToInt32(input);
+
+            char op;
+
+            do {
+                Console.Write("Enter operator[+ - * /]: ");
+                input = Console.ReadLine();
+                op = Convert.ToChar(input);
+
+                if ('+' != op && '-' != op && '*' != op && 'x' != op && 'X' != op && '/' != op) {
+                    Console.WriteLine("Invalid operator. Try again...");
+                    continue;
+                }
+                break;
+            } while (true);
+
+            double result = 0.0;
+
+            if ('+' == op) {
+                result = a + b;
+                Console.WriteLine("result = {0}", result);
+            }
+            else if ('-' == op) {
+                result = a - b;
+                Console.WriteLine("result = {0}", result);
+            }
+            else if ('*' == op || 'x' == op || 'X' == op) {
+                result = a * b;
+                Console.WriteLine("result = {0}", result);
+            }
+            else if ('/' == op) {
+                result = (double)(a) / b;
+                Console.WriteLine("result = {0}", result);
+            }
+        }
+    }
+}
