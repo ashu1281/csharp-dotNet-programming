dd05a575e05022d7879ff4eef2b6560c04031779
ashu1281 <gaiwadashish628@gmail.com>
2023-03-03 11:15:00 +0530

    recursive function

---
+++ csharp-programming/Day 02/05 recursive function/CodeFile01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace csharp_programming {
+    class Program {
+        static long Factorial(int n) {
+            if (n < 0)
+                return -1;
+
+            if (n > 10)
+                return -2;
+
+            if (1 == n)
+                return 1;
+
+            return n * Factorial(n - 1);
+        }
+
+        static void Main() {
+            long result = 0;
+            result = Factorial(4);
+            result = Factorial(-4);
+            result = Factorial(40);
+        }
+    }
+}
