b06130f754676e733676b0cc034073694c9fa9e4
ashu1281 <gaiwadashish628@gmail.com>
2023-03-03 11:15:50 +0530

    command line arguments

---
+++ csharp-programming/Day 02/09 commandline arguments/CodeFile01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace csharp_programming {
+    class Program {
+        static bool Search(int[,] arr, int value) {
+            bool found = false;
+            int rows = arr.GetLength(0), cols = arr.GetLength(1);
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+                    if (arr[row, col] == value) {
+                        found = true;
+                        goto END;
+                    }
+                }
+            }
+        END:
+            return found;
+        }
+
+        static void Main(string[] args) {
+            int[,] a = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 0, 0 } };
+            int value = Convert.ToInt32(args[0]);
+            bool found = Search(a, value);
+        }
+    }
+}
+
+/*
+ * Note args[0] is not name of an executable.
+ */
