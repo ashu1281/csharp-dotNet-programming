9113ad476917d90a041a3db76d26bff6d1ef4e62
ashu1281 <gaiwadashish628@gmail.com>
2023-03-03 11:13:34 +0530

    loops

---
+++ csharp-programming/Day 01/16 loops/CodeFile01.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace csharp_programming {
+    class Program {
+        static void Main() {
+            string input;
+
+            Console.Write("Enter integer: ");
+            input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+                result *= i;
+
+            Console.WriteLine("Factorial of {0} is {1}", n, result);
+        }
+    }
+}
---
+++ csharp-programming/Day 01/16 loops/CodeFile03.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace csharp_programming {
+    class Program {
+        static void Main() {
+            string input;
+
+            Console.Write("Enter integer: ");
+            input = Console.ReadLine();
+            int n = Convert.ToInt32(input);
+
+            long result = 1;
+
+            int i = 1;
+            do {
+                result *= i;
+                i++;
+            } while (i <= n);
+
+            Console.WriteLine("Factorial of {0} is {1}", n, result);
+        }
+    }
+}
