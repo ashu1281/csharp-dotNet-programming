d4c0eb8eedcba739b344516f23dd05ff4521ff3c
ashu1281 <gaiwadashish628@gmail.com>
2023-03-03 11:15:38 +0530

    goto

---
+++ csharp-programming/Day 02/08 goto/CodeFile01.cs
@@ -0,0 +1,24 @@
+﻿namespace csharp_programming {
+    class Program {
+        static bool Search(int[,] arr, int value) {
+            bool found = false;
+            int rows = arr.GetLength(0), cols = arr.GetLength(1);
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+                    if (arr[row, col] == value) {
+                        found = true;
+                        goto END;
+                    }
+                }
+            }
+            END:
+            return found;
+        }
+
+        static void Main() {
+            int[,] a = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 0, 0 } };
+            bool found = Search(a, 4);
+            found = Search(a, 5);
+        }
+    }
+}
