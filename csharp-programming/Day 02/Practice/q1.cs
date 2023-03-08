b8cee6594acddaadd9f1c2e808c6ecc5a87e4de1
ashu1281 <gaiwadashish628@gmail.com>
2023-03-08 12:02:48 +0530

    day2 practice

---
+++ csharp-programming/Day 02/Practice/q1.cs
@@ -0,0 +1,36 @@
+﻿//Write a method CalculateArea that takes two optional parameters viz. 
+//    width and height, and calculates the area of a rectangle. 
+//    If either parameter is not provided, it should default to 1.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_programming.Day_02.Practice
+{
+    internal class q1
+    {
+        public static double CalculateArea(double x=0, double y = 0)
+        {
+            double area = 0.0;
+            area = x * y;
+            return area;
+        }
+        public static int Main()
+        {
+            Console.Write("Enter width of Rectangle : ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Enter Height of Rectangle : ");
+            double height = double.Parse(Console.ReadLine());
+
+            double Area = CalculateArea(width, height); 
+
+            Console.WriteLine("The area of rectangle is : {0}", Area);
+            
+
+            return 0;
+        }
+    }
+}
---
+++ csharp-programming/Day 02/Practice/q2.cs
@@ -0,0 +1,36 @@
+﻿//Write a recursive function that calculates the nth number in the 
+//    Fibonacci sequence. The Fibonacci sequence is a series of numbers 
+//    where each number is the sum of the two preceding numbers, 
+//    starting with 0 and 1.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_programming.Day_02.Practice
+{
+    internal class q2
+    {
+        public static int Fibo(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return Fibo(n-1)+Fibo(n-2);
+            
+            
+        }
+        public static int Main()
+        {
+            Console.Write("Enter the nth number of series : ");
+            int n = int.Parse(Console.ReadLine());
+
+            int result = Fibo(n);
+
+            Console.Write("The {0}th number of fibonacci series is : {1}",n,result);
+            return 0;
+        }
+    }
+}
