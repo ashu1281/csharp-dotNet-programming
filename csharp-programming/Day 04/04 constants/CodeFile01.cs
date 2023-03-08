c431772f9d039826b22780bbba48d4f20b86c44f
ashu1281 <gaiwadashish628@gmail.com>
2023-03-08 12:25:46 +0530

    constants

---
+++ csharp-programming/Day 04/04 constants/CodeFile01.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_programming {
+    static class Math {
+        public const double PI = 3.14;
+    }
+    class Program {
+        static void Main() {
+            double pi = Math.PI;
+        }
+    }
+}
