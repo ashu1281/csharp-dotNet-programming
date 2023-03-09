50fa7082b0dc268c1a95e11f4ecd750e27d64303
ashu1281 <gaiwadashish628@gmail.com>
2023-03-09 11:10:15 +0530

    Revert "Revert "day5 inheritance""
    This reverts commit f5ce576cb63cf9a5119ac6505ec83c8a312e9b9f.

---
+++ csharp-programming/day 05/01 Inheritance/02 constructor order/DummyBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_programming.day_05._01_Inheritance._01_basic_class_inheritance_syntax
+{
+    internal class DummyBase
+    {
+        public DummyBase()
+        {
+            Console.WriteLine("From DummyBase Constructor");
+        }
+    }
+}
---
+++ csharp-programming/day 05/01 Inheritance/03 Passing arguments to Base Constructor/DummyBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace csharp_programming.day_05._01_Inheritance._01_basic_class_inheritance_syntax
+{
+    internal class DummyBase
+    {
+        private int i;
+        public DummyBase(int i)
+        {
+            this.i = i;
+        }
+    }
+}
