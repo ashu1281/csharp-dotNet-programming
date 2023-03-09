f82fc7057cbeae540449f0d63e4aef6637007854
ashu1281 <gaiwadashish628@gmail.com>
2023-03-09 11:07:15 +0530

    extenison method

---
+++ csharp-programming/Day 04/09 extension methods/CodeFile01.cs
@@ -0,0 +1,31 @@
+﻿namespace csharp_programming {
+    public static class StringExtensions {
+        public static bool IsPalindrome(string str) {
+            int left = 0;
+            int right = str.Length - 1;
+
+            while (left < right) {
+                if (str[left] != str[right]) {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+
+    class Program {
+        static void Main() {
+            bool result = false;
+            
+            string s = "level";
+            result = StringExtensions.IsPalindrome(s);
+
+            string t = "hello";
+            result = StringExtensions.IsPalindrome(t);
+        }
+    }
+}
---
+++ csharp-programming/Day 04/09 extension methods/CodeFile02.cs
@@ -0,0 +1,35 @@
+﻿namespace csharp_programming {
+    public static class StringExtensions {
+        public static bool IsPalindrome(this string str) {
+            int left = 0;
+            int right = str.Length - 1;
+
+            while (left < right) {
+                if (str[left] != str[right]) {
+                    return false;
+                }
+
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+
+    class Program {
+        static void Main() {
+            bool result = false;
+            
+            string s = "level";
+            result = s.IsPalindrome();
+
+            string t = "hello";
+            result = t.IsPalindrome();
+
+            result = "radar".IsPalindrome();
+
+            result = StringExtensions.IsPalindrome("racecar");
+        }
+    }
+}
