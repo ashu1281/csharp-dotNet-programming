6a6e1d5ee42ed36c005221cc725017ca3c527420
ashu1281 <gaiwadashish628@gmail.com>
2023-03-10 13:59:20 +0530

    day 06 object members overview

---
+++ csharp-programming/Day 06/01 Object Members Overview/CodeFile05.cs
@@ -0,0 +1,62 @@
+﻿namespace csharp_programming
+{
+    class Integer
+    {
+        private int value;
+
+        public Integer(int value)
+        {
+            this.value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null || obj.GetType()!= typeof(Integer))
+            {
+                return false;
+            }
+            Integer other = (Integer)obj;
+
+            return this.value == other.value;
+        }
+
+        public override int GetHashCode()
+        {
+            return value % 100;
+        }
+
+        public override string ToString()
+        {
+            return $"Value={value}";
+        }
+
+        public Integer Copy()
+        {
+            return (Integer)this.MemberwiseClone(); //member wise clone create a shallow copy
+        }
+    }
+
+    internal class Program
+    {
+        public static void Main()
+        {
+            Integer u = new Integer(2);
+            Integer v = new Integer(2);
+
+
+            Console.WriteLine(u.Equals(v));
+            Console.WriteLine(Object.Equals(u, v));
+
+            Console.WriteLine(u.GetHashCode() == v.GetHashCode());
+            Console.WriteLine(Object.ReferenceEquals(u, v));
+
+            Console.WriteLine(u.ToString());
+
+            //copy of object
+
+            Integer w = u.Copy();
+            Console.WriteLine(Object.Equals(u, w));
+            Console.WriteLine(Object.ReferenceEquals(u, w));
+        }
+    }
+}
