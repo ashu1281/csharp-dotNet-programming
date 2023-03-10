475d1830482fd2b88827157a3e60f9b6fb28e5bd
ashu1281 <gaiwadashish628@gmail.com>
2023-03-10 13:58:35 +0530

    day 05 practice

---
+++ csharp-programming/day 05/Practice/02 Practice/Tower.cs
@@ -0,0 +1,14 @@
+﻿namespace cshar_programming
+{
+    internal class Tower:IProperty
+    {
+        public Tower()
+        {
+            Render();
+        }
+        public void Render()
+        {
+            Console.WriteLine("Rendered the Property");
+        }
+    }
+}
---
+++ csharp-programming/day 05/Practice/02 Practice/TowerFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace cshar_programming
+{
+    internal class TowerFactory : IPropertyFactory
+    {
+        
+        public IProperty CreateProperty()
+        {
+            return new Tower();
+        }
+    }
+}
